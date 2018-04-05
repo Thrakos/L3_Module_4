bcbabf1cb6cd737d3fe81e62701800cf88a7ec13
Colin Sepkovic <league@WTS22.attlocal.net>
2018-04-04 18:29:48 -0700

    threadpool

--- src/threads/Jogger.java
+++ src/threads/Jogger.java
@@ -1,14 +1,14 @@
 package threads;
 
-public class Jogger implements Runnable{
+public class Jogger implements Runnable {
 
 	@Override
 	public void run() {
 		for (int i = 0; i < 1000000; i++) {
 			System.out.println("Arman");
-			System.out.print("... is the best");
+			System.out.print(" is the best");
 		}
-		
+
 	}
 
 }
